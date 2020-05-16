f1e88ac12487981e5c04c6ebf4e87201e10fa259
i-rme <i-rme@users.noreply.github.com>
2020-05-16 11:13:06 +0200

    Removed examples, created real tests

---
+++ qunit-tests.js
@@ -0,0 +1,15 @@
+
+test("Testing Worker Definition", function (assert) {
+
+    assert.equal(typeof handleRequest, "function", "The main function is defined");
+
+    assert.equal(typeof handleRequest(''), "object", "The main function returns an object");
+
+});
+
+
+test("Testing Worker Async", function (assert) {
+
+    assert.equal(handleRequest.constructor.name, "AsyncFunction", "The main function is asynchronous");
+
+});
